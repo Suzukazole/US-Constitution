88ba99833d556117ff435e310fd79669a1550155
ash <65379684+Suzukazole@users.noreply.github.com>
2021-10-04 14:50:35 +0530

    gitattr

--- kb.pl
+++ kb.pl
@@ -1,1 +1,46 @@
-ksjafiusdig
+/* 
+Name:
+ID:
+
+Name:
+ID:
+
+Name:
+ID:
+
+Name:
+ID:
+*/
+
+% ----------- Facts copied for implementation of test cases---------------
+
+age(rohan, 23).
+citizen(rohan, 23).
+age(meera, 30).
+citizen(meera, 8).
+age(david, 35).
+citizen(david, 35).
+age(leonard, 40).
+citizen(leonard, 40).
+age(amy, 38).
+citizen(amy, 5).
+stateOfUS(newHampshire).
+stateOfUS(massachusetts).
+stateOfUS(connecticut).
+stateOfUS(newYork).
+stateOfUS(newJersey).
+stateOfUS(pennsylvania).
+stateOfUS(delaware).
+stateOfUS(maryland).
+stateOfUS(virginia).
+stateOfUS(northCarolina).
+stateOfUS(southCarolina).
+stateOfUS(georgia).
+monday(7, 1, 2019).
+monday(2, 12, 2019).
+
+% Article 1
+% Section 1
+consist(congress,senate).
+consist(congress,houseOfRepresentatives).
+composed(X,Y,Z) :- consist(X,Y), consist(X,Z).
